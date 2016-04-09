fb1d1f94a471aa1c85e2b44d1707a25582d9dd0b
Moises Sanabria <sesmoi.data@gmail.com>
2016-04-09 10:56:36 -0400

    working keyDown enter post feature

--- client/components/AddPost/AddPost.js
+++ client/components/AddPost/AddPost.js
@@ -7,15 +7,30 @@ class AddPost extends React.Component {
 		'addPost': PropTypes.func
 	}
 
+	handleSubmit(event, node, cb) {
+		if (event.keyCode === 13) {
+			console.log('Enter!!')
+			console.log(node)
+			cb()
+		}
+	}
+
 	render() {
 		let input
-		// console.log('ADD-POST-COMPONENT: ', this.props)
 		const { addPost } = this.props
+		// onKeyDown={this.handleSubmit(event, input)}
 		return (
 			<div className="AddPost">
-				<input ref={node => {
-					input = node
-				}}/>
+				<input 
+					ref={node => {
+						input = node
+					}}
+					onKeyDown={(event) => {
+						this.handleSubmit(event, input.value, () => {
+							addPost(input.value)
+							input.value = ''
+						})
+					}}/>
 				<button onClick={() => {
 					addPost(input.value)
 					input.value = ''
